2fa42791bc126986fd70a233fbc06d5319a365a0
svs101 <svs101@naver.com>
2019-09-14 21:13:28 +0900

    1094 막대기

---
+++ BOJ/bj_1094/bj_1094/bj_1094.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main() {
+	int X, num = 1;
+	int stick = 64, sum = 64;
+
+	std:: cin >> X;
+
+	while (sum != X) {
+		stick /= 2;
+		num++;
+
+		if (sum - stick >= X) {
+			sum -= stick;
+			num--;
+		}
+	}
+
+	std::cout << num << std::endl;
+	
+}
