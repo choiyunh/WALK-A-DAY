125a4bd91940991b50a225a8004bc4e40b52256d
svs101 <svs101@naver.com>
2019-09-15 08:32:02 +0900

    1085 직사각형 탈출

---
+++ BOJ/bj_1085.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int x, y, w, h;
+	vector<int> tmp;
+
+	std::cin >> x >> y >> w >> h;
+
+	tmp.push_back(x);
+	tmp.push_back(y);
+	tmp.push_back(w - x);
+	tmp.push_back(h - y);
+
+	sort(tmp.begin(), tmp.end());
+
+	cout << tmp[0] << endl;
+
+	return 0;
+}
