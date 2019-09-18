5e96298e47e34a67175a286b90dc5c6869656c85
svs101 <svs101@naver.com>
2019-09-18 17:45:37 +0900

    1904 01타일

---
+++ BOJ/bj_1904.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int d[1000001] = { 0 };
+
+int dp(int N) {
+	if (N == 1) return 1;
+	if (N == 2) return 2;
+	if (d[N] != 0) return d[N];
+	return d[N] = (dp(N - 1) + dp(N - 2)) % 15746;
+}
+
+int main() {
+	int N;
+	std::cin >> N;
+
+	printf("%d\n", dp(N));
+}
