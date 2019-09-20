a59ba63dcc31a7af43636d2ed0f7ac50a0602da1
choiyunh_ <svs101@naver.com>
2019-09-21 08:53:07 +0900

    1932 정수 삼각형

---
+++ BOJ/bj_1932.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::vector<std::vector<int>> t(500, std::vector<int>(500, 0));
+std::vector<std::vector<int>> d(500, std::vector<int>(500, 0));
+
+int dp(int n, int i) {
+	if (n == 0) return t[n][i];
+	if (d[n][i] != 0) return d[n][i];
+	return d[n][i] = std::max(dp(n - 1, i), dp(n - 1, i + 1)) + t[n][i];
+}
+
+int main() {
+	int n;
+	std::cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < i; j++) {
+			std::cin >> t[n-i][j];
+		}
+	}
+
+	printf("%d\n", dp(n-1,0));
+	return 0;
+}
