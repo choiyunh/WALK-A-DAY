15f0697d3cdbb30a1e81f73c2da80806ab284a88
choiyunh_ <svs101@naver.com>
2019-09-25 15:25:49 +0900

    10844 쉬운 계단 수

--- BOJ/bj_10844.cpp
+++ BOJ/bj_10844.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 
 int main() {
-	int dp[101] = { 0 };
+	long dp[101][10] = { 0 };
 	int N;
 
 	std::cin >> N;
 	
-	dp[1] = 9;
-	for (int i = 2; i <= N; i++) {
-		dp[i] = dp[i - 1] * 2 - 1;
+	for (int i = 1; i <= 9; i++) {
+		dp[1][i] = 1;
 	}
 
-	printf("%d\n", dp[N]);
+	for (int i = 2; i <= N; i++) {
+		for (int j = 0; j <= 9; j++) {
+			if (j == 0) dp[i][j] = dp[i - 1][j + 1];
+			else if (j == 9) dp[i][j] = dp[i - 1][j - 1];
+			else dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1] % 1000000000;
+		}
+	}
 
-	
+	long sum = 0;
+	for (int i = 0; i <= 9; i++)
+		sum += dp[N][i];
+
+	printf("%d\n", sum % 1000000000);
+
 	return 0;
 }
