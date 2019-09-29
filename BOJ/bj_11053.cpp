edf1548030180fb8b6cdf4313fd4fd39cf39d300
choiyunh_ <svs101@naver.com>
2019-09-29 19:28:08 +0900

    11053 가장 긴 증가하는 부분 수열

--- BOJ/bj_11053.cpp
+++ BOJ/bj_11053.cpp
@@ -1,1 +1,28 @@
 #include <iostream>
+
+int main() {
+	int n;
+	int a[1001] = { 0 };
+	int dp[1001] = { 0 };
+	int max;
+	std::cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		std::cin >> a[i];
+		dp[i] = 1;
+	}
+
+	max = dp[1];
+	for (int i = 2; i <= n; i++) {
+		for (int j = 1; j < i; j++) {
+			if (a[i] > a[j] && dp[i] <= dp[j]) {
+				dp[i] = dp[j] + 1;
+			}
+		}
+		if (max < dp[i]) max = dp[i];
+	}
+
+	printf("%d\n", max);
+
+	return 0;
+}
