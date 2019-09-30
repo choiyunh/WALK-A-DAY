ef54054185501095d3b319af4d18939bd733f2a4
choiyunh_ <svs101@naver.com>
2019-09-30 17:54:56 +0900

    11054 가장 긴 바이토닉 부분 수열

--- BOJ/bj_11054.cpp
+++ BOJ/bj_11054.cpp
@@ -2,13 +2,29 @@
 
 int main() {
 	int a[1001], dp[1001];
-	int n;
+	int n, max;
 
 	std::cin >> n;
 
 	for (int i = 1; i <= n; i++) {
 		std::cin >> a[i];
+		dp[i] = 1;
 	}
 
+	max = dp[1];
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (j < i && a[j] < a[i]) {
+				if (j == 1 || a[j] > a[j - 1])
+					dp[i]++;
+			}
+			if (j > i && a[j] < a[i])
+				if (j == 1 || a[j] < a[j - 1])
+					dp[i]++;
+		}
+		if (dp[i] > max) max = dp[i];
+	}
+
+	printf("%d\n", max);
 	return 0;
 }
