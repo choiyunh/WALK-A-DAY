a61de042f878f87280cef2739eb47f4d37c61324
choiyunh_ <svs101@naver.com>
2019-10-01 17:49:58 +0900

    11054 가장 긴 바이토닉 부분 수열

--- BOJ/bj_11054.cpp
+++ BOJ/bj_11054.cpp
@@ -1,30 +1,40 @@
 #include <iostream>
 
 int main() {
-	int a[1001], dp[1001];
+	int a[1001], dp1[1001], dp2[1001];
 	int n, max;
 
 	std::cin >> n;
 
 	for (int i = 1; i <= n; i++) {
 		std::cin >> a[i];
-		dp[i] = 1;
+		dp1[i] = 1;
 	}
 
-	max = dp[1];
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (j < i && a[j] < a[i]) {
-				if (j == 1 || a[j] > a[j - 1])
-					dp[i]++;
+	for (int i = 2; i <= n; i++) {
+		for (int j = 1; j < i; j++) {
+			if (a[j] < a[i] && dp1[j] >= dp1[i]) {
+				dp1[i] = dp1[j] + 1;
 			}
-			if (j > i && a[j] < a[i])
-				if (j == 1 || a[j] < a[j - 1])
-					dp[i]++;
 		}
-		if (dp[i] > max) max = dp[i];
 	}
 
+	for (int i = 1; i <= n; i++) 
+		dp2[i] = 1;
+
+	for (int i = n - 1; i >= 1; i--) {
+		for (int j = n; j > i; j--) {
+			if (a[j] < a[i] && dp2[j] >= dp2[i]) {
+				dp2[i] = dp2[j] + 1;
+			}
+		}
+	}
+
+	max = dp1[1] + dp2[1] - 1;
+	for (int i = 2; i <= n; i++) {
+		if (max < dp1[i] + dp2[i] - 1)
+			max = dp1[i] + dp2[i] - 1;
+	}
 	printf("%d\n", max);
 	return 0;
 }
