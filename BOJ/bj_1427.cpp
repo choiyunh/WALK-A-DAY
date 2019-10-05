91c530a51a47cf1d2576f4fc9f9a2622fb247768
choiyunh_ <svs101@naver.com>
2019-10-05 15:54:47 +0900

    1427 소트인사이드

---
+++ BOJ/bj_1427.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string s;
+
+	std::cin >> s;
+
+	for (int i = 0; i < s.length(); i++) {
+		int max = i;
+		for (int j = i+1; j < s.length(); j++) {
+			if (s[j] > s[max]) max = j;
+		}
+		int temp = s[i];
+		s[i] = s[max];
+		s[max] = temp;
+	}
+
+	printf("%s\n", s.c_str());
+
+}
