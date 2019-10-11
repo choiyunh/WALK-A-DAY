e6c6189794e68f14762395e3d4a93915b19581c4
choiyunh_ <svs101@naver.com>
2019-10-11 13:49:58 +0900

    bj_2751 수 정렬하기2(Quick Sort)

---
+++ BOJ/bj_2751.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+//quick sort
+
+int arr[1000000];
+
+void quick_sort(int i, int j) {
+	if (i >= j) return;
+	int pivot = arr[(i+j) / 2];
+	int left = i;
+	int right = j;
+
+	while (left <= right) {
+		while (arr[left] < pivot)left++;
+		while (arr[right] > pivot) right--;
+
+		if (left <= right) {
+			std::swap(arr[left], arr[right]);
+			left++;
+			right--;
+		}
+	}
+}
+
+int main() {
+	int n;
+
+	std::cin >> n;
+
+	for (int i = 0; i < n; i++)
+		std::cin >> arr[i];
+
+	quick_sort(0, n - 1);
+
+	for (int i = 0; i < n; i++)
+		printf("%d\n", arr[i]);
+
+	return 0;
+}
