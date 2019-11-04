4eaa1cd46965ad69326ace48ecfdd8f59d62c010
choiyunh_ <svs101@naver.com>
2019-11-04 17:23:48 +0900

    19/11/04 solved largestNum.cpp - Programmers_Sort_2

--- Programmers/Sort/largestNum.cpp
+++ Programmers/Sort/largestNum.cpp
@@ -4,24 +4,25 @@
 
 using namespace std;
 
-bool cmp(const string& a, const string& b){
-	if ((a + b) > (b + a)) return true;
-	else return false;
+bool cmp(string a, string b) {
+	return a + b > b + a;
 }
-
 string solution(vector<int> numbers) {
-	string answer = "";
+	string answer;
+	vector<string> temp;
 
-	vector<string> vs;
-
-	for (int i = 0; i < numbers.size(); i++) {
-		vs.push_back(to_string(numbers[i]));
+	for (int i : numbers) {
+		temp.push_back(to_string(i));
 	}
 
-	sort(vs.begin(), vs.end(), cmp);
+	sort(temp.begin(), temp.end(), cmp);
 
-	for (int i = 0; i < vs.size() ; i++) {
-		answer += vs[i];
+	if (temp[0] == "0") {
+		answer += '0';
+		return answer;
+	}
+	for (string s : temp) {
+		answer += s;
 	}
 
 	return answer;
@@ -30,8 +31,16 @@ string solution(vector<int> numbers) {
 int main() {
 
 	vector<int> v = { 3, 30, 34, 5, 9 };
+	vector<string> v2 = { "3","30","34","9","5"};
+	//string s = solution(v);
+	string a = "3";
+	string b = "34";
 
-	solution(v);
+	if (a > b)
+		printf("%s\n", a.c_str());
 
+	sort(v2.begin(), v2.end(), cmp);
+
+	printf("%s\n", v2[4].c_str());
 	return 0;
 }
