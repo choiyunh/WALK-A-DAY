b43e79e63ae22a894e8ac85fa1144c2d3d5b42fb
choiyunh_ <svs101@naver.com>
2019-11-04 16:17:32 +0900

    19/11/04 modify numK.cpp - Programmers_Sort_1

--- Programmers/Sort/numK.cpp
+++ Programmers/Sort/numK.cpp
@@ -6,23 +6,23 @@ using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
 	vector<int> answer;
+	vector<int> temp;
 
-	int size = commands.size();
-
-	for (int i = 0; i < size; i++) {
-
-		vector<int> subArray;
-		subArray.assign(array.begin() + commands[i][0] - 1, array.begin() + commands[i][1]);
-		sort(subArray.begin(), subArray.end());
-		answer.push_back(subArray[commands[i][2] - 1]);
+	for (size_t i = 0; i < commands.size(); i++) {
+		temp.assign(array.begin() + (commands[i][0] - 1), array.begin() + (commands[i][1]));
+		sort(temp.begin(), temp.end());
+		answer.push_back(temp[commands[i][2] - 1]);
 	}
+
 
 	return answer;
 }
 
 int main() {
-	vector<int> array = { 1,2,3 };
-	vector<vector<int>> com;
-
+	vector<int> array = { 1,5,2,6,3,7,4 };
+	vector<vector<int>> com = { {2,5,3},{4,4,1},{1,7,3} };
+	vector<int> s = solution(array, com);
+	
+	printf("%d\n", s[0]);
 	return 0;
 }
