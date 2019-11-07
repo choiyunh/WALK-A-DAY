5d1804471ecfbf94124805ca88ba8b61ad1b2e0a
choiyunh_ <svs101@naver.com>
2019-11-07 19:43:12 +0900

    19/11/07 solved H-Index.cpp - Programers_Sort_3

--- Programmers/Sort/H-Index.cpp
+++ Programmers/Sort/H-Index.cpp
@@ -7,22 +7,26 @@ using namespace std;
 
 int solution(vector<int> citations) {
 	int answer = 0;
+	int cnt = 0;
 	sort(citations.begin(), citations.end());
 	int n = citations.size();
-
-	for (int i = 0; i < citations.size(); i++) {
-		if (citations[i] <= n - i)
-			answer = citations[i];
-	}
-
-	if (answer == 0) {
-		answer = n;
+	while(1){
+		for (int i = 0; i < n; i++) {
+			if (citations[i] >= answer)
+				cnt++;
+		}
+		if (cnt < answer) {
+			answer--;
+			break;
+		}
+		cnt = 0;
+		answer++;
 	}
 	return answer;
 }
 
 int main() {
-	vector<int> c = { 42,7,5 };
+	vector<int> c = { 0,0,0 };
 	printf("%d\n", solution(c));
 	return 0;
 }
