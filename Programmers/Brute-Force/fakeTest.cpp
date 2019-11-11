c8876093765a81b7c39b83d73ccb97ae4de4b1b5
choiyunh_ <svs101@naver.com>
2019-11-11 14:04:29 +0900

    19/11/11 solved fakeTest.cpp - Programmers_BT_1

--- Programmers/Brute-Force/fakeTest.cpp
+++ Programmers/Brute-Force/fakeTest.cpp
@@ -6,43 +6,40 @@ using namespace std;
 
 vector<int> solution(vector<int> answers) {
 	vector<int> answer;
-	vector<int> num;
-	int cnt = 0;
+	vector<int> num(3);
+	int max = 0;
 
-	for (int temp : answers) {
-		int i = 1;
-		if (i == temp)
-			cnt++;
-		i++;
-	}
-	num.push_back(cnt);
-	cnt = 0;
-	for (int i = 0; i < answers.size();i++) {
-		int temp = 2;
-		if (i % 2 == 0) {
-			temp = 2;
-		}
-		else {
-			temp = i % 9;
-		}
-		if (temp == answers[i])
-			cnt++;
-	}
-	num.push_back(cnt);
-	cnt = 0;
-	for (int temp : answer) {
-		int i = 1;
-		if (i == temp)
-			cnt++;
-	}
-	num.push_back(cnt);
-	cnt = 0;
+	int first[5] = { 1,2,3,4,5 };
+	int second[8] = { 2,1,2,3,2,4,2,5 };
+	int third[10] = { 3,3,1,1,2,2,4,4,5,5 };
 
+	for (int i = 0; i < answers.size(); i++) {
+		if (first[i % 5] == answers[i])
+			num[0]++;
+		if (second[i % 8] == answers[i])
+			num[1]++;
+		if (third[i % 10] == answers[i])
+			num[2]++;
+	}
+	max = num[0];
+	if (num[1] < num[2]) {
+		if (max < num[2])
+			max = num[2];
+	}
+	else
+		if (max < num[1])
+			max = num[1];
+	
+	for (int i = 0; i < 3; i++) {
+		if (num[i] == max) answer.push_back(i + 1);
+	}
 	return answer;
 }
 
 int main() {
-	vector<int> a = { 1,2,3,4,5 };
+	vector<int> a = { 1,3,2,4,2 };
 	vector<int> result = solution(a);
+
+	printf("%d\n", result[2]);
 	return 0;
 }
