b38b7402edda4809849034c059942889bd2ba4ef
choiyunh_ <svs101@naver.com>
2019-11-26 08:53:12 +0900

    19/11/26 solved island.cpp - Programmers_Greedy_7

--- Programmers/Greedy/island.cpp
+++ Programmers/Greedy/island.cpp
@@ -10,34 +10,41 @@ bool cmp(vector<int> a, vector<int> b) {
 
 int solution(int n, vector<vector<int>> costs) {
 	int answer = 0;
-	// check 배열을 만들어서 연결된 지점을 체크해주고
-	// 0 1이 연결되었을 때 2가 0이나 1 둘 중 한곳이랑만 연결되도 체크
+	// check 배열을 만들어서 방문 가능 체크
+	// 0 1이 연결되었을 때 2가 0이나 1 둘 중 한곳이랑만 연결될 때 다리 건설
 	// 비용 기준으로 오름차순 정렬해서 비용 적은 것부터 검사
 	vector<bool> check(4, false);
+	vector<bool> pass(costs.size(), false);
 	sort(costs.begin(), costs.end(), cmp);
-	bool flag = true;
-	int ix = 0;
-	while (true) {
-		// 두 섬 모두 연결되지 않았으면 두 섬 다 true
-		// 한 섬은 연결되지 않았는데 나머지 한 섬이 연결되어 있는경우
-		// 그 섬과 연결된
-		if (check[costs[ix][0]] == false) {
-			if (check[costs[ix][1]] == false) {
-				check[costs[ix][1]] = true;
-				check[costs[ix][0]] = true;
+	int cnt = 0;
+	
+	// 제일 비용 적은 첫번째 다리 건설하고 시작
+	check[costs[0][0]] = true;
+	check[costs[0][1]] = true;
+	pass[0] = true;
+	answer += costs[0][2];
+	cnt += 2;
+
+	// cnt -> 방문가능한 섬의 갯수
+	while (cnt < n) {
+		for (int i = 1; i < costs.size(); i++) {
+			if (pass[i]) continue;
+
+			if (!check[costs[i][0]] && check[costs[i][1]]) {
+				check[costs[i][0]] = true;
+				pass[i] = true; // 해당 cost의 섬 두개는 둘 다 방문 가능해지므로
+				cnt++;
+				answer += costs[i][2];
+				break; // 다리 건설했으면 다시 처음부터 체크
 			}
-			
-		}
-		
-		// 모든 섬이 연결되면 break
-		for (int i = 0; i < check.size(); i++) {
-			if (check[i] == false) {
-				flag = false;
-				break;
+			else if (check[costs[i][0]] && !check[costs[i][1]]) {
+				check[costs[i][1]] = true;
+				pass[i] = true; // 해당 cost의 섬 두개는 둘다 방문 가능해지므로
+				cnt++;
+				answer += costs[i][2];
+				break; // 다리 건설했으면 다시 처음부터 체크
 			}
-		}
-		if (flag == true) break;
-		
+		}		
 	}
 	return answer;
 }
