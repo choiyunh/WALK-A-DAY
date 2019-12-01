ac0d380ca2d06930fa3ce6601cbd100a702a28ef
choiyunh_ <svs101@naver.com>
2019-12-01 20:45:53 +0900

    19/12/01 solved expressN.cpp - Programmers_DP_1

--- Programmers/DP/expressN.cpp
+++ Programmers/DP/expressN.cpp
@@ -1,17 +1,9 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-using namespace std;
+#include <iostream>
 
-// n은 9이하이기 때문에 최대 NNNNNNNNN
-// num
-int get(int num){
-	int temp = 10;
-	for (int i = 0; i < 9; i++) {
-		if (num / 10 == 0) return temp;
-		else temp * 10;
-	}
-}
+using namespace std;
 
 int solution(int N, int number) {
 	int answer = 0;
@@ -19,28 +11,24 @@ int solution(int N, int number) {
 
 	if (number == N)
 		return 1;
-
+	
 	dp[1].push_back(N);
-	for (int i = 2; i < 9; i++) {
-		// dp[i-1][0]에 10 곱하고 N더하면 숫자 붙임
-		if ((10 * dp[i - 1][0]) + N == number) return i;      //숫자 붙여서 만들기
-		else dp[i].push_back((10 * dp[i - 1][0]) + N);
-
-		for (int j = 1; j < i; j++)    //연산 갯수
-		{
-			for (int k = 0; k < dp[j].size(); k++)
-			{
-				for (int p = 0; p < dp[i - j].size(); p++)
-				{
-					if (dp[j][k] * dp[i - j][p] == number) return i;
-					dp[i].push_back(dp[j][k] * dp[i - j][p]);
-
+	for (int i = 2; i < 9; i++) {       // 최솟값이 8보다 크면 -1 return
+		if ((dp[i - 1][0] * 10 + N == number)) return i;      // 숫자 붙이기(5,55,555)
+		else dp[i].push_back((N * 10) + N);
+		for (int j = 1; j < i; j++) {   // j 와 i-j
+			for (int k = 0; k < dp[j].size(); k++) {
+				for (int p = 0; p < dp[i - j].size(); p++) {
 					if (dp[j][k] + dp[i - j][p] == number) return i;
 					dp[i].push_back(dp[j][k] + dp[i - j][p]);
 
 					if (dp[j][k] - dp[i - j][p] == number) return i;
 					dp[i].push_back(dp[j][k] - dp[i - j][p]);
-					if (dp[i - j][p] != 0) {  //0으로 못나눔
+
+					if (dp[j][k] * dp[i - j][p] == number) return i;
+					dp[i].push_back(dp[j][k] * dp[i - j][p]);
+					
+					if (dp[i - j][p] != 0) {  // 분모가 0이 아닐 때
 						if (dp[j][k] / dp[i - j][p] == number) return i;
 						else if (dp[j][k] / dp[i - j][p] == 0);
 						else dp[i].push_back(dp[j][k] / dp[i - j][p]);
