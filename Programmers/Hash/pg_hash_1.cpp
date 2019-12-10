935cf151f6ad17771a87b552dd8d0c138b066a40
Choiyunh_ <svs101@naver.com>
2019-12-10 22:50:21 +0900

    2019/12/10 update failPlayer.cpp - Programmers_Hash_1
    2019/12/10 update failPlayer.cpp

--- Programmers/Hash/pg_hash_1.cpp
+++ Programmers/Hash/pg_hash_1.cpp
@@ -6,14 +6,17 @@ using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
 	string answer = "";
-	unordered_map<string, int> um;
-
+	unordered_map<string, int> um; // unordered_map 0
+        
+	// participant
 	for (int i = 0; i < participant.size(); i++) 
 		um[participant[i]]++;
 	
+	// completion
 	for (int i = 0; i < completion.size(); i++)
 		um[completion[i]]--;
 
+	// not 0 -> fail player
 	for (int i = 0; i < participant.size(); i++)
 		if (um[participant[i]] > 0) {
 			answer = participant[i];
