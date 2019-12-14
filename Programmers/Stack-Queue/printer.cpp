e463a28524cfba20bdf1f05487dc6801d96a0e44
Choiyunh_ <svs101@naver.com>
2019-12-14 20:36:52 +0900

    19/12/14 update printer.cpp - Programmers Stack-Queue 2

--- Programmers/Stack-Queue/printer.cpp
+++ Programmers/Stack-Queue/printer.cpp
@@ -6,20 +6,21 @@ using namespace std;
 
 int solution(vector<int> priorities, int location) {
 	int answer = 0;
-	queue<pair<int, int>> q;
-	priority_queue<int> pq;
+	queue<pair<int, int>> q; // 우선순위와 위치를 저장할 큐
+	priority_queue<int> pq; // 우선순위큐
 
 	int cnt = 0;
 
 	for (int i = 0; i < priorities.size(); i++) {
+		
 		q.push(pair<int, int>(i, priorities[i]));
 		pq.push(priorities[i]);
 	}
 	
-	int count = 0;
+	int count = 0; // 인쇄한 횟수
 	while (1) {
-		if (q.front().second == pq.top()) { // ÀÎ¼â
-			if (q.front().first == location) {
+		if (q.front().second == pq.top()) { // 우선순위큐의 front와 큐의 front가 같으면 인쇄
+			if (q.front().first == location) {// 단 위치가 location과 같으면 break후 count return
 				count++;
 				break;
 			}
@@ -27,7 +28,7 @@ int solution(vector<int> priorities, int location) {
 			pq.pop();
 			count++;
 		}
-		else {
+		else { // 다르면 큐의 front를 맨 뒤로 push
 			q.push(q.front());
 			q.pop();
 		}
