c4386aa8eeeb7fad3f5d8986ab0ced3b98ea1999
Choiyunh_ <svs101@naver.com>
2020-01-13 17:26:22 +0900

    20/01/13 skillTree.cpp

--- Programmers/skillTree.cpp
+++ Programmers/skillTree.cpp
@@ -8,13 +8,17 @@ int solution(string skill, vector<string> skill_trees) {
 	// "BACDE" 에서 B를 받아옴
 	// skill에서 B를 찾아
 	// 없으면 그냥 통과 인덱스가 0이어도 통과
-	// 있으면 skill에서 B보다 앞에있는 문자가 B 뒤애 있는지 찾아
+	// 있으면 skill에서 B보다 앞에있는 문자가 skill_trees[i]의 B 뒤에 있는지 찾아
 	// 있으면 불가능 없으면 통과
 	// 끝까지 통과하면 count++;
 	for (int i = 0; i < skill_trees.size(); i++) {
 		string temp = skill_trees[i];
 		for (int j = 0; j < temp.size(); j++) {
 			char c = temp[j];
+			if (skill[j] == c) {
+				if (j == 0) break;
+				
+			}
 		}
 	}
 	return answer;
