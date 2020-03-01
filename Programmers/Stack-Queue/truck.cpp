9c64d06849356c22190ab44c2fd3dfbb114cefd4
Choiyunh_ <svs101@naver.com>
2020-03-01 17:07:29 +0900

    20/03/01 solved truck.cpp - Programmers_S/Q

--- Programmers/Stack-Queue/truck.cpp
+++ Programmers/Stack-Queue/truck.cpp
@@ -7,34 +7,29 @@ using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
 	int answer = 0;
-	queue<int> onBridge;
+	queue<int> q;
 	int curWeight = 0;
 	int i = 0;
-		
-	while (1) {
-		if (onBridge.size() == bridge_length) {
-			if (curWeight == 0) {
-				break;
-			}			
-			curWeight -= onBridge.front();
-			onBridge.pop();
-		}
-		answer++;
-		if (i < truck_weights.size()) {
-			curWeight += truck_weights[i];
-			if (curWeight <= weight) {
-				onBridge.push(truck_weights[i++]);
-			}
-			else {
-				curWeight -= truck_weights[i];
-				onBridge.push(0);
-			}
-		}
-		else {
-			onBridge.push(0);
-		}
-	}
 
+	while (1) {
+		if (q.size() == bridge_length) { // 다리의 끝에 트럭이 도착했을 때 pop
+			curWeight -= q.front();
+			q.pop();
+		}
+		answer++; // 새 트럭이든 0이든 push하면 시간이 지나감 answer++
+		if (curWeight + truck_weights[i] <= weight) {// 현재 무게에 새 트럭의 무게를 더했을 때 견딜 수 있으면 (더한 값이 작거나 !!같으면!!)
+			curWeight += truck_weights[i]; // 현재 무게를 더해주고
+			q.push(truck_weights[i++]); // 새 트럭을 push할 때 i++
+			
+			if (i == truck_weights.size()) { // 마지막 트럭이 다리에 올랐으면 bridge_length를 더해주고 끝
+				answer += bridge_length;
+				break;
+			}
+		}
+		else { // 새 트럭을 견딜 수 없으면 0을 push
+			q.push(0);
+		}		
+	}
 	return answer;
 }
 
