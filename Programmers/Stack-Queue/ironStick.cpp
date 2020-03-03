d9f149ad0d2f7f9c9f98626e8db8f294599bf1fe
Choiyunh_ <svs101@naver.com>
2020-03-03 22:04:48 +0900

    20/03/03 solved ironStick.cpp - Programmser_S/Q

--- Programmers/Stack-Queue/ironStick.cpp
+++ Programmers/Stack-Queue/ironStick.cpp
@@ -1,3 +1,4 @@
+utej?
 #include <string>
 #include <vector>
 #include <iostream>
@@ -7,23 +8,24 @@ using namespace std;
 
 int solution(string arrangement) {
 	int answer = 0;
-	stack<char> s;
+	stack<char> s; // 들어올 문자를 저장할 stack
 	
 	for (int i = 0; i < arrangement.size(); i++) {
-		if (arrangement[i] == '(') {
+		if (arrangement[i] == '(') { // '('가 들어오면 push(막대의 시작이거나 레이저의 시작)
 			s.push(arrangement[i]);
 		}
-		else {
-			if (arrangement[i - 1] == '(') {
+		else { // ')'가 들어오면 앞에거를 보고 레이저인지 막대의 끝인지 판단
+			if (arrangement[i - 1] == '(') { // 레이저일때
 				s.pop();
-				answer += s.size();
+				answer += s.size(); // 스택에 (가 쌓여있는 만큼 막대의 시작이 있고 여기서 잘리므로 size만큼 더해주면 잘린기준 왼쪽 갯수
 			}
-			else if (arrangement[i - 1] == ')') {
+			else if (arrangement[i - 1] == ')') { // 막대의 끝일 때
 				s.pop();
-				answer++;
+				answer++; // 레이져 기준 왼쪽 갯수는 레이져에서 더해줬으니 막대 끝나면서 1씩 더해주면 됨
 			}
 		}
 	}
+
 	return answer;
 }
 
