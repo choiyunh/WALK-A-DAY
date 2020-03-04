25c78f915e88dc9d0c748dd0feb6cbc119425fbc
Choiyunh_ <svs101@naver.com>
2020-03-04 21:16:35 +0900

    20/03/04 solved moreSpicy.cpp - Programmers_Heap

--- Programmers/Heap/moreSpicy.cpp
+++ Programmers/Heap/moreSpicy.cpp
@@ -8,21 +8,22 @@ using namespace std;
 
 int solution(vector<int> scoville, int K) {
 	int answer = 0;
-	priority_queue<int, vector<int>, greater<int>> pq;
+	priority_queue<int, vector<int>, greater<int>> pq; // MIN HEAP (오름차순)
 
-	for (int i : scoville)
+	for (int i : scoville) // MIN HEAP에 자료 삽입
 		pq.push(i);
 
-	while (pq.top() < K) {
-		if (pq.size() == 1)
+	while (pq.top() < K) { // pq의 top이 K이상이면 끝
+		if (pq.size() == 1) // 음식을 섞어도 K이상이 안될 때
 			return -1;
-		int a = pq.top();
+		// pq의 top을 순서대로 두 개 꺼내서 (a+b*2)해서 다시 push
+		int a = pq.top(); 
 		pq.pop();
 		int b = pq.top();
 		pq.pop();
 
 		pq.push(a + (b * 2));
-		answer++;
+		answer++; // 섞은횟수 ++
 	}
 	return answer;
 
