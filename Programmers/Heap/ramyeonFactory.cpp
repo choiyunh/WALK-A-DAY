b10aa0bf6fb8f6ff3f99762c09936c8e7d50c1ca
Choiyunh_ <svs101@naver.com>
2020-03-06 23:39:05 +0900

    20/03/06 solved ramyeonFactory.cpp - Programmers_Heap

--- Programmers/Heap/ramyeonFactory.cpp
+++ Programmers/Heap/ramyeonFactory.cpp
@@ -9,16 +9,16 @@ int solution(int stock, vector<int> dates, vector<int> supplies, int k) {
 	int answer = 0;
 	priority_queue<int> pq;
 
-	int i = 0, j = 0;
+	int i = 0, j = 0; // i는 날짜(0일부터), j는 인덱스
 	while (i < k) {
-		if (i == dates[j]) {
-			pq.push(supplies[j]);
-			if (j < dates.size()-1) j++;
+		if (i == dates[j]) { // 공급날이 되면
+			pq.push(supplies[j]); // pq에 push (MAX HEAP)
+			if (j < dates.size()-1) j++; 
 		}
 
-		if (stock == 0) {
-			answer++;
-			stock += pq.top();
+		if (stock == 0) { // 밀가루가 다 떨어지면
+			answer++; 
+			stock += pq.top(); // MAX HEAP이므로 날짜 빠른게 아닌 양 많은거 부터
 			pq.pop();
 		}
 		stock--;
