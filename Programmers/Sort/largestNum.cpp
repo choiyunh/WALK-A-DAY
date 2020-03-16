358334ebcb98fc30081c2a2c6269c67c8103c944
Choiyunh_ <svs101@naver.com>
2020-03-16 23:47:42 +0900

    20/03/16 solved largestNum.cpp - Programmers_Sort

--- Programmers/Sort/largestNum.cpp
+++ Programmers/Sort/largestNum.cpp
@@ -4,24 +4,24 @@
 
 using namespace std;
 
-bool cmp(string a, string b) { // 큰 순서로 
-	return a + b > b + a;
-}
+bool cmp(string a, string b) { // 내림차순 정렬하는데 a>b하면 1,10 중 10이 먼저옴
+	return a + b > b + a;  // 큰 수를 만드려면 1이 먼저 오게 정렬해야하므로
+}			       // a+b와 b+a중 큰 게 먼저. (1,6같은 수는 a>b 든 a+b>b+a든 6이 먼저오니까 무관) 
 string solution(vector<int> numbers) {
 	string answer;
 	vector<string> temp;
 
-	for (int i : numbers) {
+	for (int i : numbers) { // number의 수를 string으로 변환하여 temp에 저장
 		temp.push_back(to_string(i));
 	}
 
-	sort(temp.begin(), temp.end(), cmp);
+	sort(temp.begin(), temp.end(), cmp); // 정렬
 
-	if (temp[0] == "0") {
+	if (temp[0] == "0") {  // 첫 글자가 0이면 예외처리
 		answer += '0';
 		return answer;
 	}
-	for (string s : temp) {
+	for (string s : temp) { // 정렬된 대로 수를 붙여서 answer만듬
 		answer += s;
 	}
 
