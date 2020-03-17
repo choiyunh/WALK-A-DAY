9419a20946d0255a9fd85442e0e5d43592315ae2
Choiyunh_ <svs101@naver.com>
2020-03-17 19:13:15 +0900

    20/03/17 solved H-Index.cpp - Programmers_Sort

--- Programmers/Sort/H-Index.cpp
+++ Programmers/Sort/H-Index.cpp
@@ -6,27 +6,22 @@
 using namespace std;
 
 int solution(vector<int> citations) {
-	int answer = 0;
-	int cnt = 0;
-	sort(citations.begin(), citations.end());
-	int n = citations.size();
+    
+	int answer = 0; // 몇 번 인용했는지 0부터 증가
+	int cnt = 0; // answer번 이상 인용된 논문의 갯수
+	sort(citations.begin(), citations.end()); // 오름차순 정렬
+	int n = citations.size(); // n
 	while(1){
-		for (int i = 0; i < n; i++) {
-			if (citations[i] >= answer)
+		for (int i = 0; i < n; i++) {  
+			if (citations[i] >= answer) // answer번 이상 인용되었으면 cnt++
 				cnt++;
 		}
-		if (cnt < answer) {
-			answer--;
-			break;
+		if (answer > cnt) { // answer번 이상 인용된 논문이 answer개보다 적으면
+			answer--; 
+			break; // break
 		}
 		cnt = 0;
-		answer++;
+		answer++; // answer을 1씩 증가하면서 진행
 	}
 	return answer;
-}
-
-int main() {
-	vector<int> c = { 0,0,0 };
-	printf("%d\n", solution(c));
-	return 0;
 }
