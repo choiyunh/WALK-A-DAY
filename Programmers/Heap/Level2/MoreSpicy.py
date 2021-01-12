7299c872ec33313a525f79b8349238b4c3277684
Choiyunh_ <svs101@naver.com>
2021-01-12 23:10:57 +0900

    2020/12/28 solved More Spicy

---
+++ Programmers/Heap/Level2/MoreSpicy.py
@@ -0,0 +1,21 @@
+import heapq
+
+
+def solution(scoville, K):
+    answer = 0
+    hq = []
+
+    for i in scoville:
+        heapq.heappush(hq, i)
+
+    while hq[0] < K:
+        try:
+            heapq.heappush(hq, heapq.heappop(hq) + (heapq.heappop(hq) * 2))
+        except IndexError:
+            return -1
+        answer += 1
+
+    return answer
+
+
+print(solution([1, 2, 3, 9, 10, 12], 7))
