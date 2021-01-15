82365023e7e188c10e82b63f26d61d22a242f459
Choiyunh_ <svs101@naver.com>
2021-01-15 13:34:11 +0900

    21/01/15 solved Largest Number

--- Programmers/Sort/Level2/LargestNumber.py
+++ Programmers/Sort/Level2/LargestNumber.py
@@ -1,27 +1,20 @@
 import functools
 
-
+# 값을 바꾸고 싶을 때 (위치를 바꾸고 싶을 때) 1을 리턴하고
+# 그냥 그대로 가고 싶으면 -1을 리턴
 # a+b < b+a -> 1
 # a+b > b+a -> -1
 def compare(a, b):
     return (b + a > a + b) - (a + b > b + a)
 
-
-# def cmp(a, b):
-#     return (b > a) - (a > b)
-
-
 def solution(numbers):
     answer = ''
-
-    # temp = sorted(numbers, key=functools.cmp_to_key(cmp))
-    # print(temp)
 
     temp = [str(n) for n in numbers]
     temp = sorted(temp, key=functools.cmp_to_key(compare))
 
     if temp[0] == '0':
-        return "0"
+        return '0'
 
     for s in temp:
         answer += s
