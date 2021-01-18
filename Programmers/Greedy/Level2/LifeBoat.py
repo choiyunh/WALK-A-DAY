4e4f2c91d791805e631b12436d7a60e151c3f8c4
Choiyunh_ <svs101@naver.com>
2021-01-18 12:43:40 +0900

    21/01/08 solved LifeBoat with python3

---
+++ Programmers/Greedy/Level2/LifeBoat.py
@@ -0,0 +1,11 @@
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    i = 0
+    j = len(people) - 1
+    while i <= j:
+        if people[i] + people[j] <= limit:
+            i += 1
+        j -= 1
+        answer += 1
+    return answer
