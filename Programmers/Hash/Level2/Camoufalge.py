df591f2b83853731c56f058bd57fb16fd8b274a1
Choiyunh_ <svs101@naver.com>
2021-02-01 12:13:22 +0900

    21/02/01 solved NumK(K번째 수)

---
+++ Programmers/Hash/Level2/Camoufalge.py
@@ -0,0 +1,24 @@
+def solution(clothes):
+    answer = 1
+    dic = {}
+    for cloth in clothes:
+        if cloth[1] in dic:
+            dic[cloth[1]] += 1
+        else:
+            dic[cloth[1]] = 1
+    #print(dic)
+
+    # 2+1 * 1+1 = 3
+    # (yellow_hat, green_turban, 0) (blue_sunglasses, 0)
+    # 3*2 = 6
+    # 6 - 1
+
+    for num in dic.values():
+        answer *= num + 1
+
+    return answer - 1
+
+
+print(solution([['yellow_hat', 'headgear'],
+          ['blue_sunglasses', 'eyewear'],
+          ['green_turban', 'headgear']]))
