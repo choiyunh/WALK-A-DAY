e1180aeebc4cde41a0d790f833342051e1c24016
Choiyunh_ <svs101@naver.com>
2021-03-02 17:24:18 +0900

    21/03/02 solved N개의 최소공배수

---
+++ Programmers/Practice/Level2/LCM.py
@@ -0,0 +1,20 @@
+def solution(arr):
+    n = 1
+    while True:
+        temp = arr[-1] * n
+        n += 1
+
+        flag = True
+        for a in arr[:-1]:
+            if temp % a != 0:
+                flag = False
+                break
+
+        if flag:
+            answer = temp
+            break
+
+    return answer
+
+
+print(solution([2, 6, 8, 14]))
