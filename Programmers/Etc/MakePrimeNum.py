21a4df14f6dfef58e17f54963825e4b04aa22b44
Choiyunh_ <svs101@naver.com>
2021-03-11 00:24:15 +0900

    21/03/10 solved 소수 만들기

--- Programmers/Etc/MakePrimeNum.py
+++ Programmers/Etc/MakePrimeNum.py
@@ -3,19 +3,18 @@ import math
 
 
 def isPrime(num):
-    for i in range(2, (int)(math.sqrt(num)) + 1):
-        if (num % i == 0):
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
             return False
-
     return True
 
 
 def solution(nums):
     answer = 0
     comList = list(combinations(nums, 3))
-
+    # print(comList)
     for item in comList:
-        if (isPrime(sum(item))):
+        if isPrime(sum(item)):
             answer += 1
     return answer
 
