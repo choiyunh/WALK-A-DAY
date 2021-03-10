5f66e5445846d1d71f628757a8d652597f0c9cef
Choiyunh_ <svs101@naver.com>
2021-03-10 23:58:56 +0900

    21/03/10 solved 폰켓몬

--- Programmers/Etc/Ponketmon.py
+++ Programmers/Etc/Ponketmon.py
@@ -1,3 +1,17 @@
 def solution(nums):
     answer = 0
+    cnt = len(nums) // 2
+    temp = []
+
+    for n in nums:
+        if not cnt:
+            break
+        if not temp or n not in temp:
+            answer += 1
+            cnt -= 1
+        temp.append(n)
+
     return answer
+
+
+print(solution([3, 3, 3, 3]))
