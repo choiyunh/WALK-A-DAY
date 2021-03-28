df6321dff1e25933b85008211eef9872b2124fde
Choiyunh_ <svs101@naver.com>
2021-03-28 18:00:24 +0900

    Move file

---
+++ Programmers/Stack-Queue/Level2/truck.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+	int answer = 0;
+	queue<int> q;
+	int curWeight = 0;
+	int i = 0;
+
+	while (1) {
+		if (q.size() == bridge_length) { // 다리의 끝에 트럭이 도착했을 때 pop
+			curWeight -= q.front();
+			q.pop();
+		}
+		answer++; // 새 트럭이든 0이든 push하면 시간이 지나감 answer++
+		if (curWeight + truck_weights[i] <= weight) {// 현재 무게에 새 트럭의 무게를 더했을 때 견딜 수 있으면
+			curWeight += truck_weights[i]; // 현재 무게를 더해주고
+			q.push(truck_weights[i++]); // 새 트럭을 push할 때 i++
+
+			if (i == truck_weights.size()) { // 마지막 트럭이 다리에 올랐으면 bridge_length를 더해주고 끝
+				answer += bridge_length;
+				break;
+			}
+		}
+		else { // 새 트럭을 견딜 수 없으면 0을 push
+			q.push(0);
+		}
+	}
+	return answer;
+}
