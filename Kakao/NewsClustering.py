cb56faf995f337caf1fb24451b927f32dc6ed702
Choiyunh_ <svs101@naver.com>
2021-04-03 23:28:45 +0900

    21/04/03 solved [1차] 뉴스 클러스터링

---
+++ Kakao/NewsClustering.py
@@ -0,0 +1,36 @@
+def solution(str1, str2):
+    str1 = str1.upper()
+    str2 = str2.upper()
+    rst1, rst2 = [], []
+
+    for s, part in zip(str1, str1[1:]):
+        temp = s + part
+        if temp.isalpha():
+            rst1.append(temp.lower())
+
+    for s, part in zip(str2, str2[1:]):
+        temp = s + part
+        if temp.isalpha():
+            rst2.append(temp.lower())
+
+    realUnion = set(rst1 + rst2)
+    lenInter, lenUnion = 0, 0
+    # print(rst1, rst2)
+    # print(realUnion)
+    for r in realUnion:
+        if r in rst1 and r in rst2:
+            lenInter += min(rst1.count(r), rst2.count(r))
+        lenUnion += max(rst1.count(r), rst2.count(r))
+    if lenUnion == 0:
+        return 65536
+    answer = int(lenInter / lenUnion * 65536)
+    return answer
+
+
+print(solution('aa1+aa2', 'AAAA12'))
+
+# {1,1,2,2,3} {1,2,2,4,5}
+# {1,2,3,4,5}
+
+# {1,2} {1,3}
+# {1, 2, 3}
