3acb7280072895c63a04be85cf9c3b96d4671e94
Choiyunh_ <svs101@naver.com>
2021-04-05 18:24:37 +0900

    21/04/05 solved 후보키 - 2019 KAKAO BLIND

---
+++ Kakao/CandidateKey.py
@@ -0,0 +1,42 @@
+from itertools import combinations
+
+
+def solution(relation):
+    answer = 0
+    row = len(relation)
+    col = len(relation[0])
+
+    candiKey = []
+
+    attribute = [a for a in range(col)]
+
+    for n in range(1, col + 1):
+        combiList = []
+        combiList += list(combinations(attribute, n))
+        for i in range(len(combiList)):
+            combiList[i] = set(combiList[i])
+
+        print(combiList)
+        print(candiKey)
+        for ck in candiKey:
+            for ix in range(len(combiList)):
+                if len(ck & combiList[ix]) == len(ck):
+                    print('교', ck & combiList[ix])
+                    combiList[ix] = set()
+        print(combiList)
+        for cl in combiList:
+            temp = set()
+            for r in range(row):
+                key = []
+                for cc in cl:
+                    key.append(relation[r][cc])
+                temp.add(tuple(key))
+            print(temp)
+            if len(temp) == row:
+                candiKey.append(cl)
+                answer += 1
+    return answer
+
+
+print(solution([["100", "ryan", "music", "2"], ["200", "apeach", "math", "2"], ["300", "tube", "computer", "3"],
+                ["400", "con", "computer", "1"], ["500", "muzi", "music", "3"], ["600", "apeach", "music", "2"]]))
