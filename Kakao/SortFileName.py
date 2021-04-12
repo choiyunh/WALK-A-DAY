f314a9104f5b885d29a388b99c854b55c024bb14
Choiyunh_ <svs101@naver.com>
2021-04-12 21:26:09 +0900

    21/04/12 solved 파일명 정렬 - 2018 BLIND

--- Kakao/SortFileName.py
+++ Kakao/SortFileName.py
@@ -1,27 +1,27 @@
 def solution(files):
-    dic = {}
+    answer = []
 
     for file in files:
         flag = True
         startNum = 0
+        tmp = []
         for i in range(len(file)):
             if flag and file[i].isdigit():
-                dic[file] = [file[:i]]
+                tmp = [file[:i]]
                 startNum = i
                 flag = False
             if not flag:
                 if not file[i].isdigit():
-                    dic[file].append(file[startNum:i])
-                    dic[file].append(file[i:])
+                    tmp.append(file[startNum:i])
+                    tmp.append(file[i:])
                     break
                 elif i == len(file) - 1:
-                    dic[file].append(file[startNum:])
+                    tmp.append(file[startNum:])
                     break
-    print(dic)
-    dic = sorted(dic.items(), key=lambda x: (x[1][0].lower(), int(x[1][1])))
+        answer.append(tmp)
 
-    answer = [d[0] for d in dic]
-    return answer
+    answer.sort(key=lambda x: (x[0].lower(), int(x[1])))
+    return [''.join(a) for a in answer]
 
 
 print(solution(["img12", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]))
