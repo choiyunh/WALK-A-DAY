7c58408d6d9194c283cc3970b5be9a933f0c80d0
Choiyunh_ <svs101@naver.com>
2021-04-27 20:48:30 +0900

    21/04/27 solved 기능개발

---
+++ Programmers/Stack-Queue/Level2/DevFunc.py
@@ -0,0 +1,17 @@
+def solution(progresses, speeds):
+    answer = []
+    days = []
+
+    for p, s in zip(progresses, speeds):
+        day = -((p - 100) // s)
+        if not len(days) or day > days[-1]:
+            days.append(day)
+            answer.append(1)
+        else:
+            answer[-1] += 1
+    return answer
+
+
+print(solution([2, 2, 1, 2], [1, 1, 1, 1]))
+
+# 98 98 99 98
