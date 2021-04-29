3aa4fb2613b783b9f18d55c1add238fc3b7857f6
Choiyunh_ <svs101@naver.com>
2021-04-29 20:44:20 +0900

    21/04/29 solved K번째 수

--- Programmers/Sort/Level1/NumK.py
+++ Programmers/Sort/Level1/NumK.py
@@ -1,11 +1,7 @@
 def solution(array, commands):
     answer = []
-
     for c in commands:
-        temp = array[c[0] - 1:c[1]]
-        temp.sort()
-        answer.append(temp[c[2] - 1])
-
+        answer.append(sorted(array[c[0]-1:c[1]])[c[2]-1])
     return answer
 
 
