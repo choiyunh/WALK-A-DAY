79f56ce9d554cff43a4a2282d81ea74b6b769a94
Choiyunh_ <svs101@naver.com>
2021-05-31 20:00:52 +0900

    21/05/31 solved 가장 큰 정사각형 찾기

--- Programmers/Practice/Level2/LargestSquare.py
+++ Programmers/Practice/Level2/LargestSquare.py
@@ -1,15 +1,19 @@
 def solution(board):
     answer = 0
 
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] == 1:
-                x, y = j + 1, i + 1
-                while board[i][x] != 0 and board[y][j] != 0 and x < len(board[0]) and y < len(board):
-                    x, y = x + 1, y + 1
+    if len(board) == 1:
+        return int(1 in board[0])
+    elif len(board[0]) == 1:
+        return int([1] in board)
 
-                while board[]
-    return answer
+    for i in range(1, len(board)):
+        for j in range(1, len(board[0])):
+            if board[i][j]:
+                board[i][j] = min(board[i - 1][j], board[i][j - 1], board[i - 1][j - 1]) + 1
+                answer = max(board[i][j], answer)
+
+    return answer * answer
 
 
 print(solution([[0, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 1, 0]]))
+print(solution([[0, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])) # ???
