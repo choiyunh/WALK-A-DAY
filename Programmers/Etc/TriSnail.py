08c5d74eef1b3a77727effa9f3075b8d71526f61
Choiyunh_ <svs101@naver.com>
2021-06-10 21:23:17 +0900

    21/06/10 solved 삼각 달팽이

--- Programmers/Etc/TriSnail.py
+++ Programmers/Etc/TriSnail.py
@@ -1,54 +1,43 @@
 def solution(n):
     snail = [[0 for i in range(n)] for i in range(n)]
 
-    num = 1
+    # 삼각 달팽이에 채울 숫자의 최대값
+    maxNum = n * (n // 2 + 1)
+    if not n & 1:
+        maxNum -= (n // 2)
+
+    top, bottom, left, right = 0, n - 1, 0, n - 1
     flag = 0
-    minRow = 1
-    maxRow = n - 1
-    maxCol = n - 1
-    row = 0
-    col = 0
-    while True:
+    row, col = -1, 0
+    for i in range(1, maxNum + 1):
         if flag == 0:
-            if snail[row][col] != 0:
-                break
-            snail[row][col] = num
-            if row == maxRow:
+            row += 1
+            if row == bottom:
                 flag = 1
-                maxRow -= 1
-                col += 1
-            else:
-                row += 1
+                top += 1
+                left += 1
         elif flag == 1:
-            if snail[row][col] != 0:
-                break
-            snail[row][col] = num
-            if col == maxCol:
+            col += 1
+            if col == right:
                 flag = 2
-                maxCol -= 1
-                row -= 1
-                col -= 1
-            else:
-                col += 1
+                bottom -= 1
+                right -= 1
         elif flag == 2:
-            if snail[row][col] != 0:
-                break
-            snail[row][col] = num
-            if row == minRow:
+            row -= 1
+            col -= 1
+            if row == top or col == left:
                 flag = 0
-                minRow += 1
-                maxCol -= 1
-                row += 1
-            else:
-                row -= 1
-                col -= 1
-        num += 1
+                top += 1
+                right -= 1
+        snail[row][col] = i
+
     answer = []
-    for r in snail:
-        for c in r:
-            if c:
-                answer.append(c)
+    for i in range(n):
+        for j in range(n):
+            if snail[i][j]:
+                answer.append(snail[i][j])
+
     return answer
 
 
-print(solution(6))
+print(solution(5))
