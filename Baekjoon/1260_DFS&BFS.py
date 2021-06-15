5924f2a7424d80b6d12e7d4c5eaa9e14e72c0610
Choiyunh_ <svs101@naver.com>
2021-06-15 18:08:17 +0900

    21/06/15 solving DFS&BFS

--- Baekjoon/1260_DFS&BFS.py
+++ Baekjoon/1260_DFS&BFS.py
@@ -1,20 +1,51 @@
-n, m, v = input().split()
+from collections import deque
 
-graph = [[0] * (int(n) + 1) for _ in range(int(n) + 1)]
-visited = []
+n, m, v = map(int, input().split())
+graph = {}
 
-for _ in range(int(m)):
+for _ in range(m):
     a, b = input().split()
-    graph[int(a)][int(b)] = 1
+    a, b = int(a), int(b)
+    if a in list(graph.keys()):
+        if b not in graph[a]:
+            graph[a].append(b)
+    else:
+        graph[a] = [b]
+
+    if b in list(graph.keys()):
+        if a not in graph[b]:
+            graph[b].append(a)
+    else:
+        graph[b] = [a]
 
 
 def dfs(v):
-    visited.append(v)
-    for i in range(1, int(n) + 1):
-        if i not in visited and graph[v][i] == 1:
-            dfs(i)
+    stack = [v]
+    visited = []
+
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
+            if node in graph:
+                temp = sorted(graph[node], reverse=True)
+                stack.extend(temp)
+    return visited
 
 
-dfs(int(v))
-print(visited)
-visited = []
+def bfs(v):
+    queue = deque([v])
+    visited = []
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.append(node)
+            if node in graph:
+                temp = sorted(graph[node])
+                queue.extend(temp)
+    return visited
+
+
+print(dfs(v))
+print(bfs(v))
