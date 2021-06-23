f1739fb5d5c049ea1247b57a34795910abcda7cd
Choiyunh_ <svs101@naver.com>
2021-06-24 01:40:52 +0900

    21/06/24 solving 효율적인 해킹

--- Baekjoon/1325_효율적인 해킹.py
+++ Baekjoon/1325_효율적인 해킹.py
@@ -8,3 +8,27 @@ for _ in range(m):
         graph[a] = [b]
 
 print(graph)
+
+while stack:
+    n = stack.pop()
+    if not answer[n]:
+        answer[n] = p
+        if n in graph:
+            temp = sorted(graph[n], reverse=True)
+            stack.extend(temp)
+
+print(graph)
+print(answer)
+
+def dfs(v):
+    stack = [v]
+    visited = []
+
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
+            if node in graph:
+                temp = sorted(graph[node], reverse=True)
+                stack.extend(temp)
+    return visited
