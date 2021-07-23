49b056ee26092b424af57f842d5b1bb7cd4542d0
Choiyunh_ <svs101@naver.com>
2021-07-23 23:36:43 +0900

    21/07/19 solved 스택 수열

--- Baekjoon/자료구조/1966_프린터 큐.py
+++ Baekjoon/자료구조/1966_프린터 큐.py
@@ -1,2 +1,14 @@
+from collections import deque
+
 N = int(input())
 
+answer = []
+for _ in range(N):
+    nDoc, num = map(int, input().split())
+    q = deque(input().split())
+
+    while True:
+        if q and q[0] == min(q):
+            q.popleft()
+        else:
+            q.append(q.popleft())
