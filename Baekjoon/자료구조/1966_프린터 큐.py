45e054fc0db490c521444c49877864f2647cf164
Choiyunh_ <svs101@naver.com>
2021-07-27 20:32:42 +0900

    21/07/27 solved 프린터 큐

--- Baekjoon/자료구조/1966_프린터 큐.py
+++ Baekjoon/자료구조/1966_프린터 큐.py
@@ -5,18 +5,23 @@ N = int(input())
 answer = []
 for _ in range(N):
     nDoc, num = map(int, input().split())
-    q = deque(input().split())
+    printList = input().split()
+
+    q = deque()
+    for i in range(len(printList)):
+        q.append((i, printList[i]))
 
     target = q[num]
     cnt = 0
     while True:
-        if q[0] == max(q):
+        if q[0][1] == max(printList):
             if q[0] == target:
                 cnt += 1
                 print(cnt)
                 break
             else:
                 cnt += 1
+                printList.remove(q[0][1])
                 q.popleft()
         else:
             temp = q.popleft()
