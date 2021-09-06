aba4f451e3050dffa1961ad5b67f1ba8e24d6283
Choiyunh_ <svs101@naver.com>
2021-09-06 22:10:29 +0900

    21/09/06 LeetCode-Search Insert Position(Binary Search)

--- Leetcode/Easy/SearchInsertPosition.py
+++ Leetcode/Easy/SearchInsertPosition.py
@@ -9,8 +9,16 @@ You must write an algorithm with O(log n) runtime complexity.
 
 class Solution:
     def searchInsert(self, nums: [int], target: int) -> int:
-        if target in nums:
-            return nums.index(target)
-        else:
-            nums.append(target)
-            return sorted(nums).index(target)
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            middle = (left + right) >> 1
+            if nums[middle] == target:
+                return middle
+            if target < nums[middle]:
+                right = middle - 1
+            else:
+                left = middle + 1
+        return left
