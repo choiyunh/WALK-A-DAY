9d7fb6edc46e77f8833ae2705e64fb83bacc71b7
Choiyunh_ <svs101@naver.com>
2021-09-11 22:07:44 +0900

    21/09/11 LeetCode - Sqrt(x)

--- Leetcode/Easy/Sqrt(x).py
+++ Leetcode/Easy/Sqrt(x).py
@@ -1,2 +1,33 @@
+import math
+
+
 class Solution:
-    def mySqrt(self, x: int) -> int:
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        # approach: use binary search to find the largest int whose
+        #           square is less or equal than x
+
+        left, right = 0, x
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if mid * mid > x:
+                right = mid - 1
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                return mid
+
+        # left > right
+        return right
+
+# OR
+#
+# class Solution:
+#     def mySqrt(self, x: int) -> int:
+#         answer = int(math.sqrt(x))
+#         return answer
