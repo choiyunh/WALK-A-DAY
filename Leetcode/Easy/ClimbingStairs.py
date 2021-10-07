fcb6c0f3b9ea54773f8de841b9f44437aab54dec
Choiyunh_ <svs101@naver.com>
2021-10-07 23:36:33 +0900

    21/10/07 solved Climbing Stairs(DP)

--- Leetcode/Easy/ClimbingStairs.py
+++ Leetcode/Easy/ClimbingStairs.py
@@ -1,12 +1,21 @@
 class Solution:
     cache = {}
+
     def climbStairs(self, n: int) -> int:
         if n < 3:
             return n
-        else :
-            return self._climbStairs(n-1) + self._climbStairs(n-2)
+        else:
+            return self._climbStairs(n - 1) + self._climbStairs(n - 2)
 
     def _climbStairs(self, n: int):
-        if n not in self.cache.keys() :
+        if n not in self.cache:
             self.cache[n] = self.climbStairs(n)
         return self.cache[n]
+
+############################### USE BRUTE FORCE
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         if n < 3:
+#             return n
+#         else :
+#             return self.climbStairs(n-1) + self.climbStairs(n-2)
