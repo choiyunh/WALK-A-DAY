733e19098db20dc4d18a6f6e0647376dcf8e9d4a
Choiyunh_ <svs101@naver.com>
2021-12-02 18:39:47 +0900

    21/12/02 프로그래머스/2018 KAKAO BLIND RECRUITMENT/[3차] 압축

---
+++ Kakao/Compression.py
@@ -0,0 +1,27 @@
+def solution(msg):
+    answer = []
+    dic = {}
+
+    for i in range(1, 27):
+        dic[chr(64 + i)] = i
+
+    i = 0
+    msg += '-'
+    while i < len(msg) - 1:
+        if i + 1 == len(msg):
+            answer.append(dic[msg[i]])
+            break
+        j = 2
+        w = msg[i:i + j]
+        while w in dic:
+            j += 1
+            w = msg[i:i + j]
+
+        answer.append(dic[w[:-1]])
+        dic[w] = len(dic) + 1
+        i += (len(w) - 1)
+
+    return answer
+
+
+print(solution("TOBEORNOTTOBEORTOBEORNOT"))
