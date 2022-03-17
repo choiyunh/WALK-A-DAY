ef59fbd515219957df97378de77c6eddc553695a
Choiyunh_ <svs101@naver.com>
2022-03-17 20:01:57 +0900

    22/03/17 solving BOJ-1092-배

---
+++ Baekjoon/Greedy/1092_배.py
@@ -0,0 +1,23 @@
+n = int(input())
+crane = sorted(list(map(int, input().split())), reverse=True)
+
+m = int(input())
+box = sorted(list(map(int, input().split())), reverse=True)
+
+result = 0
+count = 0
+
+positions = [0] * n
+checked = [0] * m
+
+while count < len(box):
+    for i in range(n):
+        while positions[i] < len(box):
+            if not checked[positions[i]] and crane[i] >= box[positions[i]]:
+                checked[positions[i]] = True
+                positions[i] += 1
+                count += 1
+                break
+            positions[i] += 1
+    result += 1
+print(result)
