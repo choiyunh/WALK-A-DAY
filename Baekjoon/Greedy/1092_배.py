3c223c71541aff382d2e796e8ceb4a9cc519c5af
Choiyunh_ <svs101@naver.com>
2022-03-18 23:37:27 +0900

    22/03/18 solved BOJ-1092-배

--- Baekjoon/Greedy/1092_배.py
+++ Baekjoon/Greedy/1092_배.py
@@ -10,14 +10,17 @@ count = 0
 positions = [0] * n
 checked = [0] * m
 
-while count < len(box):
-    for i in range(n):
-        while positions[i] < len(box):
-            if not checked[positions[i]] and crane[i] >= box[positions[i]]:
-                checked[positions[i]] = True
+if box[0] > crane[0]:
+    print(-1)
+else:
+    while count < len(box):
+        for i in range(n):
+            while positions[i] < len(box):
+                if not checked[positions[i]] and crane[i] >= box[positions[i]]:
+                    checked[positions[i]] = True
+                    positions[i] += 1
+                    count += 1
+                    break
                 positions[i] += 1
-                count += 1
-                break
-            positions[i] += 1
-    result += 1
-print(result)
+        result += 1
+    print(result)
